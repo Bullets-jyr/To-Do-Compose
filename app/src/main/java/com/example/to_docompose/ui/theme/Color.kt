ec1d8ac208606c3336f302c56fecf59710d48661
Bullets-jyr <jyr@bullets.co.kr>
2023-08-14 18:29:16 +0900

    22. List Screen - List App Bar Actions

--- app/src/main/java/com/example/to_docompose/ui/theme/Color.kt
+++ app/src/main/java/com/example/to_docompose/ui/theme/Color.kt
@@ -18,9 +18,9 @@ val MediumPriorityColor = Color(0xFFFFC114)
 val HighPriorityColor = Color(0XFFFF4646)
 val NonePriorityColor = Color(0xFFFFFFFF)
 
-//val Colors.fabBackgroundColor: Color
-//    @Composable
-//    get() = if (isLight) Teal200 else Purple700
+val Colors.fabBackgroundColor: Color
+    @Composable
+    get() = if (isLight) Teal200 else Purple700
 
 val Colors.topAppBarContentColor: Color
     @Composable

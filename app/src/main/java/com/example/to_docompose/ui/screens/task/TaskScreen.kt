2ac8849358e948a1144f2826a00eaa19fc3f529e
Bullets-jyr <jyr@bullets.co.kr>
2023-08-15 14:08:46 +0900

    29. Task Screen - New Task App Bar

---
+++ app/src/main/java/com/example/to_docompose/ui/screens/task/TaskScreen.kt
@@ -0,0 +1,60 @@
+package com.example.to_docompose.ui.screens.task
+
+import androidx.compose.material.Scaffold
+import androidx.compose.runtime.Composable
+
+@Composable
+fun TaskScreen(
+//    selectedTask: ToDoTask?,
+//    sharedViewModel: SharedViewModel,
+//    navigateToListScreen: (Action) -> Unit
+) {
+//    val title: String by sharedViewModel.title
+//    val description: String by sharedViewModel.description
+//    val priority: Priority by sharedViewModel.priority
+
+//    val context = LocalContext.current
+
+    Scaffold(
+        topBar = {
+//            TaskAppBar(
+//                selectedTask = selectedTask,
+//                navigateToListScreen = { action ->
+//                    if (action == Action.NO_ACTION) {
+//                        navigateToListScreen(action)
+//                    } else {
+//                        if (sharedViewModel.validateFields()) {
+//                            navigateToListScreen(action)
+//                        } else {
+//                            displayToast(context = context)
+//                        }
+//                    }
+//                }
+//            )
+        },
+        content = {
+//            TaskContent(
+//                title = title,
+//                onTitleChange = {
+//                    sharedViewModel.updateTitle(it)
+//                },
+//                description = description,
+//                onDescriptionChange = {
+//                    sharedViewModel.description.value = it
+//                },
+//                priority = priority,
+//                onPrioritySelected = {
+//                    sharedViewModel.priority.value = it
+//                }
+//            )
+        }
+    )
+}
+
+//fun displayToast(context: Context) {
+//    Toast.makeText(
+//        context,
+//        "Fields Empty.",
+//        Toast.LENGTH_SHORT
+//    ).show()
+//}
